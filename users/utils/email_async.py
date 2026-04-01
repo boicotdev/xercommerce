7d8ac6110d78903b021ad5184a3aadbf6006b696
boicotdev <carlos.guzman93@live.com>
2026-03-31 20:40:43 -0500

    Celery has been added to the project

--- users/utils/email_async.py
+++ users/utils/email_async.py
@@ -2,7 +2,6 @@ import threading
 
 
 def run_async(func):
-
     def wrapper(*args, **kwargs):
         thread = threading.Thread(target=func, args=args, kwargs=kwargs)
         thread.daemon = True
